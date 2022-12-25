c59d836824b69693fb9792dc207947915a2ab6cd
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2022-12-25 18:29:00 +0530

    modified week 5 project 2 and 3

--- Articles/array_methods.js
+++ Articles/array_methods.js
@@ -1,5 +1,5 @@
 let array1 = ['mango', 25, true, 'ball', 2.5];
-console.log(array1);
+// console.log(array1);
 
 // Methods
 
@@ -123,7 +123,75 @@ console.log(array1);
 // console.log(find2);
 // console.log(array1);
 
-// sort()
-let sort1 = array1.sort();
-console.log(sort1);
+// // sort()
+// let sort1 = array1.sort();
+// console.log(sort1);
+// console.log(array1);
+
+// // Array.from()
+// let str2 = 'jeetu Dewangan';
+// let new1 = Array.from(str2);
+// console.log(new1);
+
+// // Practices Practices Practices Practices Practices Practices Practices
+// let slice1 = array1.slice(300, 3002);
+// console.log(slice1);
+// console.log(array1);
+
+// let splice1 = array1.splice(2, 0, "new-2", 'new-3')
+// console.log(splice1);
+// console.log(array1);
+
+// let includes1 = array1.includes('25'); // ===
+// console.log(includes1);
+
+// array1.push(false);
+// let sort1 = array1.sort();
+// console.log(sort1);
+// let reverse1 = array1.reverse();
+// console.log(reverse1);
 console.log(array1);
+
+// let fill1 = array1.fill(35,'jj', 1); // fill(<value>, start, end)
+// console.log(array1);
+
+// delete(array1[2])
+// console.log(array1);
+// console.log(array1[2]);
+
+// let join1 = array1.join(' +++ ')
+// console.log(join1);
+
+// let string1 = 'https://course.masaischool.com/quizzes/581'
+// let split1 = string1.split('/')
+// // split1[1] = split1[1].split('/')
+// // split1[0].concat(split1[1].split('/'))
+// console.log(split1);
+
+// // array1.unshift(42)
+// let find1 = array1.findIndex((ele) => ele>18);
+// console.log(find1);
+
+let arr2 = [52, 53, 54, 60]
+// let reduce1 = arr2.reduce((ele1, ele2) => ele1 < ele2)
+// console.log(reduce1);
+
+// let filter1 = arr2.filter(ele => ele>58)
+// console.log(filter1);
+
+// let every1 = arr2.some(ele=>55>ele)
+// console.log(every1);
+
+// let num1 = 'aa2';
+// console.log(typeof num1); 
+
+// for (let ele of array1){      // for ___ of
+//     if (typeof ele == 'boolean') {
+//         console.log(ele);
+//     }
+// }
+
+let arr3 = [2 ,[5, ['web'], 'dev'], 2.5]
+console.log(arr3[1][1][0]);
+
+
