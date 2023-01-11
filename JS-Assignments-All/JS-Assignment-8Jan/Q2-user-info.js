2fd6d9b81fd604dfa0f22f9057b89b146ca68796
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-01-11 22:21:34 +0530

    JS-Assignment 8jan-2nd Completed

---
+++ JS-Assignments-All/JS-Assignment-8Jan/Q2-user-info.js
@@ -0,0 +1,11 @@
+let myName = "Ajay";
+let lastName = "Dewangan";
+let married = false;
+let country = "India";
+let age = 26;
+
+console.log(`My Name is ${myName} ${lastName} \n
+I am married = ${married} \n
+My country is ${country} \n
+My age is ${age} \n
+`);
---
+++ JS-Assignments-All/JS-Assignment-8Jan/readme.md
@@ -0,0 +1,5 @@
+# JS-Assignment 8-Jan All Solutions Here
+
+## Q.2 - 
+
+## Q.1 - [variables.js](variable.js)
