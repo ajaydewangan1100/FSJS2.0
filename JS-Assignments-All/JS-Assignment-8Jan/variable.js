daaaeb8f51b6578a20be9ef58db2578f374b4bfc
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-01-11 22:07:09 +0530

    JS-Assignment 8jan-1st Completed

---
+++ JS-Assignments-All/JS-Assignment-8Jan/variable.js
@@ -0,0 +1,19 @@
+let myVar1 = "Tri";
+let myVar2 = 420;
+let myVar3 = true;
+let myVar4;
+let myVar5 = undefined;
+let myVar6 = null;
+
+console.log(`Type of ${myVar1}, is - ${typeof myVar1}\n
+Type of ${myVar2}, is - ${typeof myVar2}\n     
+Type of ${myVar3}, is - ${typeof myVar3}\n    
+Type of ${myVar4}, is - ${typeof myVar4}\n    
+Type of ${myVar5}, is - ${typeof myVar5}\n    
+Type of ${myVar6}, is - ${typeof myVar6}\n    
+`);
+
+
+
+
+// node JS-Assignments-All/JS-Assignment-8Jan/variable.js
