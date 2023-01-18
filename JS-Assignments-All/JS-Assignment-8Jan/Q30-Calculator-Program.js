8e6fd841c740d3a490ad6aba94dd5991aa00a670
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-01-18 19:35:09 +0530

    JS assignment 8Jan all questions completed

--- JS-Assignments-All/JS-Assignment-8Jan/Q30-Calculator-Program.js
+++ JS-Assignments-All/JS-Assignment-8Jan/Q30-Calculator-Program.js
@@ -1,9 +1,26 @@
 // Q30 Simple Calculator Program - - - - - - - - - - - - - - - - - - - - - - - 
 
-
-let expression = '45+3-2' // String(prompt('Enter your Calculation values (eg- 45x3+5):'));
-
-function evaluateExpression(expression) {
-  return eval(expression);
+function evaluateMe(){
+  let expression = String(prompt('Enter your Calculation values (eg- 45*3+5):'));
+  expression = expression.replace(/\s/g,'');
+  console.log(eval(expression));
+  document.write(eval(expression));
 }
-console.log(evaluateExpression(expression));
+evaluateMe();
+
+// let expression = '45.7+3-2';
+// expression = expression.replace(/\s/g,'');
+// let numArray = [];
+// let signArray = [];
+// let prev = 0;
+// for (let i = 0; i < expression.length; i++){
+//   if(isNaN(expression[i]) && expression[i] != '.'){
+//     numArray.push(parseFloat(expression.slice(prev, i)));
+//     numArray.push(expression[i]);
+//     prev = ++i;
+//   }
+//   if(i == expression.length - 1){
+//     numArray.push(parseFloat(expression.slice(prev)));
+//   }
+// }
+// console.log(numArray);
