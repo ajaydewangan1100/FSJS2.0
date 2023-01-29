1a59a186becef4bcfad614e5f10d026db90b4a7b
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-01-29 23:21:55 +0530

    Calculator program updated

--- JS-Assignments-All/03-Class-Assignment-22Jan/Calculator/script.js
+++ JS-Assignments-All/03-Class-Assignment-22Jan/Calculator/script.js
@@ -10,6 +10,8 @@
 	let allExpression = [];
 	let equalToken = false;
 	let bracesToken = 0;
+	let percentToken = true;
+	let 
 
 	// Loop for all buttons Eventlistener
 	for(let i = 0; i < allButtons.length; i++){
@@ -18,12 +20,17 @@
 
 	// Function for changing data 
 	function changeData(useme) {
+		if(percentToken == false){
+			typingValues.value = "";
+			percentToken = true;
+		}
 
 		if (allExpression.length == 0 || equalToken == true){typingValues.value = "";}
 		equalToken = false;
 		
 		// Condition for All Clear
 		if (allButtons[useme].value == "erase"){
+			percentToken = true;
 			typingValues.value = "";
 			paraShow.innerHTML = "";
 			allExpression = [] ;
@@ -41,7 +48,7 @@
 		}
 
 		// Condition for Percentage ---- %
-		else if(allButtons[useme].value === "%") {
+		else if(allButtons[useme].value === "%" && percentToken == true) {
 			percentageInput(useme);
 		}
 
@@ -57,8 +64,13 @@
 
 		// Condition for submission ----- "=" 
 		else if (allButtons[useme].value == "=") {
+			for(let i = 0; i < bracesToken; i++){
+				typingValues.value += ")";
+			}
 			
+			let lll = eval(typingValues.value);
 			paraShow.innerHTML = typingValues.value;
+			alert(lll);
 			typingValues.value = eval(typingValues.value);
 			equalToken = true;
 			bracesToken = 0;
@@ -68,6 +80,10 @@
 
 	// function for number - input
 	function numberInput(useme) {
+		if(String(typingValues.value[typingValues.value.length-1]) === ")"){
+			typingValues.value += '*';
+			allExpression += '*';
+		}
 		if(String(allButtons[useme].value) === ')'){
 			typingValues.value += '*';
 			allExpression += '*';
@@ -88,10 +104,17 @@
 			let checkMe = typingValues.value[typingValues.value.length-1];
 
 			if(typingValues.value.length !== 0) {
-				if(typeof(Number(checkMe)) !== "NaN" || String(checkMe) === ')'){
-					typingValues.value += '*';
-					allExpression += '*';
+				if(checkMe === '('){
+					typingValues.value += '(';
+					allExpression += '(';
+					bracesToken++;
 				}
+				else if(typeof(Number(checkMe)) !== "NaN" || String(checkMe) === ')'){
+					typingValues.value += '*(';
+					allExpression += '*(';
+					bracesToken++;
+				}
+				
 			}
 			if(checkMe === '+' || checkMe === '-' || checkMe === '*' || checkMe === '/'){
 				bracesToken += 1;
@@ -107,19 +130,27 @@
 				}
 			}
 		}
+		if(typingValues.value.length == 0){
+			typingValues.value += '(';
+			allExpression += '(';
+			bracesToken++;
+		}
 	}
 
 	// Funciton for Percentage
 	function percentageInput(useme){
+		percentToken = false;
 		let checkMe = typingValues.value[typingValues.value.length-1];
 		let percent = typingValues.value.length-1;
-		if(typeof(Number(checkMe)) === "number"){
-			for (let percent = typingValues.value.length-1; (typingValues.value[percent] !== undefined || typingValues.value[percent] !== "NaN"); percent-- ) {
-				if(Number(typingValues.value[percent] === "NaN")){
-					break;
-				}
-			}
-			alert(typingValues.value.slice(percent));
+		if(Number(typingValues.value[percent]) >= 0 && Number(typingValues.value[percent]) <= 9){ 
+			
+			while (Number(typingValues.value[percent]) >= 0 && Number(typingValues.value[percent]) <= 9) {
+				percent--;
+			} ;
+			percent++;
+
+			const per = 1 * Number(typingValues.value.slice(percent)) / 100;
+			typingValues.value = typingValues.value.slice(0, percent) + per;
 		}
 	}
 
