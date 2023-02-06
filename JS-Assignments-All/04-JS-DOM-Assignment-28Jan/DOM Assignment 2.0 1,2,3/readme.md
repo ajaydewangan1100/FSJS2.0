b4afc1849542aca027a2c7f1e0748b9b3dfca235
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-02-06 23:27:58 +0530

    Dom 28Jan submitted

--- JS-Assignments-All/04-JS-DOM-Assignment-28Jan/DOM Assignment 2.0 1,2,3/readme.md
+++ JS-Assignments-All/04-JS-DOM-Assignment-28Jan/DOM Assignment 2.0 1,2,3/readme.md
@@ -185,7 +185,17 @@ Output Task-7 - 2nd
 code --
 ```
 document.querySelector(".main__form-btn").disabled = false;
-document.querySelector(".main__form-input").disabled = false;
+document.querySelector(".main__form-input").value = "iNeuron";
+
+document.querySelector("form").addEventListener("submit", (event) => {
+        let allElements = document.querySelectorAll(".main__languages>a");
+        for(let element of allElements){
+            if(element.innerText.includes("2.0")){element.style.display = "inline"}
+        }
+        
+        document.querySelector(".main__form-input").value = "";
+        event.preventDefault();
+    })
 ```
 
 
--- JS-Assignments-All/04-JS-DOM-Assignment-28Jan/DOM P7/DOM P7/script.js
+++ JS-Assignments-All/04-JS-DOM-Assignment-28Jan/DOM P7/DOM P7/script.js
@@ -15,6 +15,15 @@ func1(useMe);
 
 // submit and refresh
 document.querySelector(".main__form-btn").disabled = false;
-document.querySelector(".main__form-input").disabled = false;
-// document.querySelector(".main__form-input").value = "iNeuron";
+document.querySelector(".main__form-input").value = "iNeuron";
+
+document.querySelector("form").addEventListener("submit", (event) => {
+        let allElements = document.querySelectorAll(".main__languages>a");
+        for(let element of allElements){
+            if(element.innerText.includes("2.0")){element.style.display = "inline"}
+        }
+        
+        document.querySelector(".main__form-input").value = "";
+        event.preventDefault();
+    })
 
