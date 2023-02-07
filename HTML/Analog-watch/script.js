5e6a69c46f1f814348ac24b8795e6faaf68729a0
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-02-07 12:01:49 +0530

    Analog Clock complete

--- HTML/Analog-watch/script.js
+++ HTML/Analog-watch/script.js
@@ -1,11 +1,10 @@
 
 setInterval(() => {
     const timeIs = new Date();
-
+    
     document.getElementsByClassName("second")[0].style.transform = "rotate(" + timeIs.getSeconds() * 6 + "deg)";
 
     document.getElementsByClassName("minute")[0].style.transform = "rotate(" + timeIs.getMinutes() * 6 + "deg)";
 
-    document.getElementsByClassName("hour")[0].style.transform = "rotate(" + timeIs.getHours() * 30 + Math.floor(timeIs.getMinutes()/2) + "deg)";
-
-}, 1000);
+    document.getElementsByClassName("hour")[0].style.transform = `rotate(${timeIs.getHours() * 30 + 30 / 60 * timeIs.getMinutes()}deg)`;
+}, 500);
---
+++ HTML/Flip-Game-hitesh/script.js
@@ -0,0 +1,6 @@
+
+let img_1 = document.getElementById("img_1");
+
+let color = window.getComputedStyle(img_1).backgroundColor;
+
+console.log(color.background);
