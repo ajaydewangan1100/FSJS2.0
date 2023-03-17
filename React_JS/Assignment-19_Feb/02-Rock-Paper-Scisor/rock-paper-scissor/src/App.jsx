fe2eb9ee73eab4dcf6ee5476258fe0281abc9d21
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-03-17 12:06:45 +0530

    rock paper scissor started

--- React_JS/Assignment-19_Feb/02-Rock-Paper-Scisor/rock-paper-scissor/src/App.jsx
+++ React_JS/Assignment-19_Feb/02-Rock-Paper-Scisor/rock-paper-scissor/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import FirstPage from './components/FirstPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Rock Paper Scissor</h1>
+    <div className="m-auto text-center">
+      <h1 className='inline bg-gray-500'>Rock Paper Scissor</h1>
+      <div className='w-[98%] h-[100%] m-auto flex flex-wrap justify-around p-5 gap-6 sm:gap-1 '>
+        <FirstPage  />
+          <div className='border border-gray-500 border-1 block w-[80%] sm:w-[2px] h-[2px] sm:h-[80vh] '></div>
+        <FirstPage user="true" />
+      </div>
     </div>
   )
 }
