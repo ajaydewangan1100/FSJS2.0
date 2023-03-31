779f7ef31af9dac3b5cc56a1877c97941fb9f035
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-03-31 13:50:54 +0530

    Todo with redux modified

---
+++ React_JS/Assignment-19_Feb/todo_with_redux/src/components/TodoForm.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const TodoForm = () => {
+  return (
+    <div>
+        <form >
+            <input type='text' />
+            <button type='submit' > Add </button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
---
+++ React_JS/Assignment-19_Feb/todo_with_redux/src/components/TodoList.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <div>TodoList showing here via useSelector</div>
+  )
+}
+
+export default TodoList
