c931303d1992fa986e17db36d3dd1e5cac7351d3
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-04-13 19:23:26 +0530

    DB connection and express check and listening done

---
+++ Live-mega-app/ecom-app/src/app.js
@@ -0,0 +1,10 @@
+// getting express make it required for our project
+const express = require('express');
+
+// and here initalizing express 
+const app = express();
+
+
+
+// export spp for external use
+export default app;
---
+++ Live-mega-app/ecom-app/src/index.js
@@ -0,0 +1,29 @@
+const { config } = require("dotenv");
+import app from "./app.js";
+const mongoose = require("mongoose");
+
+
+
+( async () => {
+    try {
+        // Make DB work await always
+        await mongoose.connect(config.MONGODB_URL);
+        console.log("DB CONNECTED !");
+
+        app.on('error', (err) => {
+            console.log("ERROR: ", err);
+            throw err
+        } )
+
+        const onListening = () => {
+            console.log(`Listening on port: ${config.PORT}`);
+        }
+        // listen take a callbackfunction we provide it by variable
+        app.listen(config.PORT, onListening)
+        
+        
+    } catch (err) {
+        console.error("ERROR: ", err)
+        throw err
+    }
+} )()
